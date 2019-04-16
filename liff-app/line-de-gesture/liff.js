c3416970516c2884298a0099c521de06bf7928dd
Koki Miura <memjoalty@yahoo.co.jp>
2019-04-17 01:57:53 +0900

    Fix led toggle function

--- liff-app/line-de-gesture/liff.js
+++ liff-app/line-de-gesture/liff.js
@@ -243,7 +243,7 @@ function liffToggleDeviceLedState(state) {
     // on: 0x01
     // off: 0x00
     window.ledCharacteristic.writeValue(
-        state ? new Unit8Array([0x01]) : new Uint8Array([0x00])
+        state ? new Uint8Array([0x01]) : new Uint8Array([0x00])
     ).catch(error => {
         uiStatusError(makeErrorMsg(error), false);
     });
