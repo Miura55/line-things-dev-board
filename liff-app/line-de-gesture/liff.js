48ad38b23996d40bd52d3bb164a6036495e657b7
Koki Miura <memjoalty@yahoo.co.jp>
2019-04-20 17:35:01 +0900

    Chnage sw

--- liff-app/line-de-gesture/liff.js
+++ liff-app/line-de-gesture/liff.js
@@ -221,7 +221,7 @@ function liffGetButtonStateCharacteristic(characteristic) {
             const buffer = new DataView(e.target.value.buffer);
             const sw1 = buffer.getInt16(0, true);
             const sw2 = buffer.getInt16(2, true);
-            if (sw1 == 0x0001) {
+            if (sw1 > 0　|| sw2 > 0) {
                 // press
                 uiToggleStateButton(true);
             } else {
