3612c17513b7787d9962b31d04007642582c3ade
Koki Miura <memjoalty@yahoo.co.jp>
2019-04-21 00:30:03 +0900

    Changed UUID

--- liff-app/line-de-gesture/liff.js
+++ liff-app/line-de-gesture/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '0867e4e2-ad87-4fd2-b2b4-9e2fbba3e814'; // LED, Button
+const USER_SERVICE_UUID         = 'ae8edba0-a010-44ba-bfd6-913754414ca1'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const NOTIFY_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
