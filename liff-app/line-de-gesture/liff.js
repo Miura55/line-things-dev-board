cf3d32f25d94e3a95b09f38f83ccaecb40ced103
Koki Miura <memjoalty@yahoo.co.jp>
2019-04-20 14:59:58 +0900

    add buffer

--- liff-app/line-de-gesture/liff.js
+++ liff-app/line-de-gesture/liff.js
@@ -218,8 +218,9 @@ function liffGetButtonStateCharacteristic(characteristic) {
     // (Get notified when button state changes)
     characteristic.startNotifications().then(() => {
         characteristic.addEventListener('characteristicvaluechanged', e => {
-            const sw1 = (new Uint8Array(e.target.value.buffer))[0];
-            const sw2 = (new Uint8Array(e.target.value.buffer))[2];
+            const buffer = DataView(e.target.value.buffer);
+            const sw1 = buffer.getInt16(0, true);
+            const sw2 = buffer.getInt16(2, true);
             if (sw1 > 0　|| sw2 > 0) {
                 // press
                 uiToggleStateButton(true);
