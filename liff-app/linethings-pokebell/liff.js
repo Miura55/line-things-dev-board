2cf517828238f982fe3a8662d681f9926367a62e
Koki Miura <memjoalty@yahoo.co.jp>
2019-05-01 18:12:40 +0900

    Chnage valuex

--- liff-app/linethings-pokebell/liff.js
+++ liff-app/linethings-pokebell/liff.js
@@ -263,7 +263,7 @@ function liffToggleDeviceLedState(state) {
     // on: 0x01
     // off: 0x00
     window.ledCharacteristic.writeValue(
-        state ? new Uint8Array([0x32]) : new Uint8Array([0x00])
+        state ? new Uint8Array([0x02]) : new Uint8Array([0x00])
     ).catch(error => {
         uiStatusError(makeErrorMsg(error), false);
     });
