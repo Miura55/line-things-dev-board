7ac114af5c1babeefecbfef06626b0c2162da27e
Koki Miura <memjoalty@yahoo.co.jp>
2019-07-09 01:12:56 +0900

    Fix for handson

--- liff-app/linethings-starter/liff.js
+++ liff-app/linethings-starter/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '8a4a71d7-d73c-4d02-8067-57e69880a3a0'; // LED, Button
+const USER_SERVICE_UUID         = 'ff011918-61b9-4282-a3f8-1bceba200e5f'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
